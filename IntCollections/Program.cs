f1653ce0457c840224a211d8b0fa909a6439ea05
ivyycore <mariasequeira.dev@gmail.com>
2025-05-09 13:37:52 +0100

    Created Listsd

--- IntCollections/Program.cs
+++ IntCollections/Program.cs
@@ -8,10 +8,12 @@ namespace IntCollections
     {
         private static void Main(string[] args)
         {
-        private static void Main(string[] args)
-        {
+            int[] array = {1, 10, -30, 10, -5};
 
-        }
+            List<int> list = new List<int>();
+            Stack<int> stack = new Stack<int>();
+            Queue<int> queue = new Queue<int>();
+            HashSet<int> hashSet = new HashSet<int>();
         }
     }
 }
