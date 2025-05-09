756e67fa8c39be0e851a12add5ac7848d546bb34
ivyycore <mariasequeira.dev@gmail.com>
2025-05-09 13:51:11 +0100

    Created FilePower2

---
+++ FilePower2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FilePower2
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
